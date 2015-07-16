6597312b9cdf6a98424cdc2713feabf3e8ef11dd
TrungLe <itrung.uit@gmail.com>
2015-07-16 08:56:16 +0700

    update project file

---
+++ GameDx/GameDx/InformationResource.h
@@ -0,0 +1,10 @@
+
+
+#ifndef __INFOMATIONRESOURCE_H__
+#define __INFOMATIONRESOURCE_H__
+
+/************RESOURCE FOR PLAYER************/
+const char* rockmanCollide = "Image\Game\Sprites\RockMan\RockManNormal"
+
+/*******************************************/
+#endif
