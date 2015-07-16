2f5f19f3327e249b1b1af1154141a15ee95d79f8
diemhuongpie <truongdiemhuong94@gmail.com>
2015-07-16 15:23:48 +0700

    fix bug header Information Resource

---
+++ GameDx/GameDx/InformationResource.cpp
@@ -0,0 +1,22 @@
+#include "Game\Utill\InformationResource.h"
+
+/************RESOURCE FOR PLAYER************/
+const wchar_t*    CInfomationResource::rockmanHit				= L"Resource//Image//Game//Sprites//RockMan//RockManNormal//rockman_collide.png";
+const wchar_t*    CInfomationResource::rockmanDie				= L"Resource//Image//Game//Sprites//RockMan//RockManNormal//rockman_die.png";
+const wchar_t*    CInfomationResource::rockmanJump				= L"Resource//Image//Game//Sprites//RockMan//RockManNormal//rockman_jump.png";
+const wchar_t*    CInfomationResource::rockmanJumpShoot			= L"Resource//Image//Game//Sprites//RockMan//RockManNormal//rockman_jump_fire.png";
+const wchar_t*    CInfomationResource::rockmanRun				= L"Resource//Image//Game//Sprites//RockMan//RockManNormal//rockman_run.png";
+const wchar_t*    CInfomationResource::rockmanRunShoot			= L"Resource//Image//Game//Sprites//RockMan//RockManNormal//rockman_run_fire.png";
+const wchar_t*    CInfomationResource::rockmanClimb				= L"Resource//Image//Game//Sprites//RockMan//RockManNormal//rockman_stair.png";
+const wchar_t*    CInfomationResource::rockmanClimbEnd			= L"Resource//Image//Game//Sprites//RockMan//RockManNormal//rockman_stair_end.png";
+const wchar_t*    CInfomationResource::rockmanClimbShoot		= L"Resource//Image//Game//Sprites//RockMan//RockManNormal//rockman_stair_fire.png";
+const wchar_t*    CInfomationResource::rockmanStand				= L"Resource//Image//Game//Sprites//RockMan//RockManNormal//rockman_stand.png";
+const wchar_t*    CInfomationResource::rockmanStandShoot		= L"Resource//Image//Game//Sprites//RockMan//RockManNormal//rockman_stand_fire";
+const wchar_t*    CInfomationResource::rockmanStart				= L"Resource//Image//Game//Sprites//RockMan//RockManNormal//rockman_start.png";
+
+/*******************************************/
+
+/************RESOURCE FOR SELECTSTAGE*******/
+const wchar_t*    CInfomationResource::backgroundSeletStage		= L"Resource//Image//SelectStage//selectStage.png";
+const wchar_t*    CInfomationResource::buttonMenuState			= L"Resource//Image//SelectStage//button_menu_state.png";
+/******************************************/
