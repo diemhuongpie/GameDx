9c9e679b59951b2357aff93dd99e88c41bef88ec
TrungLe <itrung.uit@gmail.com>
2015-08-20 11:59:06 +0700

    NEw BombBullet

---
+++ GameDx/GameDx/Class/Game/Entity/Bullet/BombBullet.cpp
@@ -0,0 +1,11 @@
+#include "BombBullet.h"
+
+
+CBombBullet::CBombBullet()
+{
+}
+
+
+CBombBullet::~CBombBullet()
+{
+}
---
+++ GameDx/GameDx/Class/Game/Entity/Bullet/BombBullet.h
@@ -0,0 +1,8 @@
+#pragma once
+class CBombBullet
+{
+public:
+	CBombBullet();
+	~CBombBullet();
+};
+
